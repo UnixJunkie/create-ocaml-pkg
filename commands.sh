efec48d929d979444e17b9bfb520be6b60f187f2
Francois Berenger <berenger@riken.jp>
2014-03-31 10:18:17 +0900

    corrected tar command

--- commands.sh
+++ commands.sh
@@ -3,7 +3,7 @@
 # requires fpm for package creation; cf. https://github.com/jordansissel/fpm
 
 wget http://caml.inria.fr/pub/distrib/ocaml-4.01/ocaml-4.01.0.tar.gz
-tar ocaml-4.01.0.tar.gz
+tar xzf ocaml-4.01.0.tar.gz
 cd ocaml-4.01.0
 ./configure -prefix /usr/local/ocaml-4.01.0
 make opt.opt
